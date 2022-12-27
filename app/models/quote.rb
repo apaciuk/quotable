6337f640124b55cda4f75e07ce1eecf43bfc8981
Paul Anthony McGowan <xhostcom@aol.com>
2022-12-27 15:11:40 +0000

    Update

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -8,11 +8,14 @@
 #  updated_at :datetime         not null
 #
 class Quote < ApplicationRecord
-    validates :name, presence: true
+    validates :name, presence: true, length: { minimum: 3, maximum: 100 }
     belongs_to :user, optional: true
+    before_create { |quote| quote.name = quote.name.titleize  }
 
     scope :ordered, -> { order(id: :desc) }
     
     broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+
+   
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
 end
 
   resources :notifications, only: [:index]
-  resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
 end
