75bba172d7c48e5943126d964ce7556863e47627
Paul Anthony McGowan <xhostcom@aol.com>
2023-01-02 16:09:28 +0000

    Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,10 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  if ENV["REDISCLOUD_URL"]
+    uri = URI.parse(ENV["REDISCLOUD_URL"])
+    $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  end
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
